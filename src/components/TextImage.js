7d920d8af490a8d7bf5db040ee5091910cae083f
mushtaque12 <48409277+mushtaque12@users.noreply.github.com>
2019-09-19 15:17:39 +0100

    image style
    image\

--- src/components/TextImage.js
+++ src/components/TextImage.js
@@ -9,9 +9,31 @@ class TextImage extends Component {
   }
 
   componentDidMount() {
+    const wrapText = (canvasTxt, text, x, y, maxWidth, lineHeight) => {
+      var words = text.split(" ");
+      var line = "";
+      console.log(x);
+      for (let n = 0; n < words.length; n++) {
+        let testLine = line + words[n] + " ";
+        let metrics = canvasTxt.measureText(testLine);
+        let testWidth = metrics.width;
+        if (testWidth > maxWidth && n > 0) {
+          canvasTxt.fillText(line, x, y);
+          line = words[n] + " ";
+          y += lineHeight;
+        } else {
+          line = testLine;
+        }
+      }
+      canvasTxt.fillText(line, x, y);
+    };
     let canvasTxt = document.getElementById("canvasComponent").getContext("2d");
-    canvasTxt.canvas.width = canvasTxt.measureText(this.props.name).width;
-    canvasTxt.fillText(this.props.name, this.props.x, this.props.y);
+    let maxWidth = 500;
+    let lineHeight = 25;
+    let x = 0;
+    let y = 100;
+    canvasTxt.font = "16px Permanent Marker";
+    wrapText(canvasTxt, this.props.name, x, y, maxWidth, lineHeight);
     this.setState({
       img: canvasTxt.canvas.toDataURL()
     });
